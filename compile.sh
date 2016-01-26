873e5ce9864228d204ec324e7cc3ea6bec87d107
M. A. Farajian <ma.farajian@gmail.com>
2016-01-26 10:03:14 +0100

    cleaning some files

--- compile.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-mkdir -p bin
-
-#gcc src/bilbowa++.v0.1.c -g -o bin/bilbowa++ -lm -pthread -Ofast -march=native -Wall -funroll-loops
-gcc src/bilbowa.v0.1.c -g -o bin/bilbowa -lm -pthread -Ofast -march=native -Wall -funroll-loops
-
